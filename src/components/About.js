028e2a0ac2c5533a10155f505d2d2879d11fd994
Navaneeth V <navaneethnair69@gmail.com>
2024-07-15 11:29:11 +0530

    Update

--- src/components/About.js
+++ src/components/About.js
@@ -18,16 +18,22 @@ class About extends React.Component {
     render(){
         //console.log("Parent Render");
         return(
+
             <div>
+                
             <h1>About</h1>
+            
             <div>
 
                 {/* Way the use context used in class component */}
     LoggedIn User :
+
     <UserContext.Consumer>
         {(loggedInUser)=> <h1 className="text-xl font-bold">{loggedInUser}</h1>}
     </UserContext.Consumer>
+
             </div>
+
             <h2>This is the About Section for the food app</h2>
             
             <UserClass name = {"Navaneeth V "} Location = {"Palakkad - class"}/>
