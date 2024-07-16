6b20a1d53cad9d6a072021b627ba518a6118bcf4
Navaneeth V <navaneethnair69@gmail.com>
2024-07-16 16:21:43 +0530

    Updateing items

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -10,27 +10,40 @@ const Cart = () => {
     const dispatch = useDispatch();
 
     const handleClearCart = () => {
+
         dispatch(clearCart());
+
     };
     
    return (
-    <div className="text-center m-4 p-4">
-     <h1 className="text-2xl font-bold">Cart</h1>
-     <div className="w-6/12 m-auto">
+
+    <div 
+    className="text-center m-4 p-4">
+
+     <h1 
+     className="text-2xl font-bold">
+        Cart</h1>
+
+     <div 
+     className="w-6/12 m-auto">
      
         <button 
+
         className="p-2 m-2 bg-black text-white rounded-lg" 
+
         onClick={handleClearCart}>
             Clear Cart
+
         </button>
 
         {cartItems.length == 0 &&
          <h1>Cart Itmes are empty.Please Add items to the cart</h1>}
         <ItemList items={cartItems}/>
+        
      </div>
      </div> 
      )
 }
 
-className='w-[10] h-[100]'
+// className='w-[10] h-[100]'
 export default Cart;
